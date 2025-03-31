f9f515c7cb7bff588131964eb014882234bcc76e
Over-knight <lilvickydsecond@gmail.com>
2025-03-31 09:52:57 +0100

    Updated vote route and canged the models to only function with votingEvent models

--- server/src/models/Candidate.ts
+++ server/src/models/Candidate.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const candidateSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    department: {type: String, required: true},
-    profilePicture: {type: String},
-    manifesto: {type: String},
-    categoryId: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true},
-    votes: {type: Number, default: 0},
-    eventId: { type: mongoose.Schema.Types.ObjectId, ref: "VotingEvent", required: true}
-});
+// const candidateSchema = new mongoose.Schema({
+//     name: {type: String, required: true},
+//     department: {type: String, required: true},
+//     profilePicture: {type: String},
+//     manifesto: {type: String},
+//     categoryId: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true},
+//     votes: {type: Number, default: 0},
+//     eventId: { type: mongoose.Schema.Types.ObjectId, ref: "VotingEvent", required: true}
+// });
 
-const Candidate = mongoose.model("Candidate", candidateSchema);
-export default Candidate;
+// const Candidate = mongoose.model("Candidate", candidateSchema, "candidates");
+// export default Candidate;
