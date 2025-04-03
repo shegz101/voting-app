896fecc3e767c54d3816cd3df255f059a7cda687
Over-knight <lilvickydsecond@gmail.com>
2025-04-03 07:58:00 +0100

    you an now vot in multiple events

--- server/src/models/Voter.ts
+++ server/src/models/Voter.ts
@@ -3,17 +3,19 @@ import mongoose, {Schema, Document} from "mongoose";
 interface Ivoter extends Document {
     voterId: string;
     candidateId: mongoose.Types.ObjectId;
+    eventId: mongoose.Types.ObjectId;
     // To track when each vote was cast
     createdAt: Date;
 }
 const VoterSchema = new Schema<Ivoter>({
     voterId: {type: String, required: true},
     candidateId: {type: Schema.Types.ObjectId, ref: "Candidate", required: true},
+    eventId: {type: Schema.Types.ObjectId, required: true},
     //To store the voting time
     createdAt: { type: Date, default: Date.now }
 });
 // Ensure a voter can only vote once per category
-VoterSchema.index({ voterId: 1, categoryId: 1}, {unique: true});
+VoterSchema.index({ voterId: 1, eventId: 1}, {unique: true});
 
 const Voter = mongoose.model<Ivoter>("Voter", VoterSchema);
 export default Voter;
