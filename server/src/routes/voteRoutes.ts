69c2771f8ae526f7c9e0850ba0c15721a1b92d11
Over-knight <lilvickydsecond@gmail.com>
2025-04-04 13:15:28 +0100

    this should work

--- server/src/routes/voteRoutes.ts
+++ server/src/routes/voteRoutes.ts
@@ -13,9 +13,7 @@ router.post(
       }
       const { voterId } = req.body;
       if (!voterId) {
-        return res
-          .status(400)
-          .json({ error: "voterId (matric number) is required" });
+        return res.status(400).json({ error: "voterId (matric number) is required" });
       }
 
       const event = await VotingEvent.findOne({
@@ -24,15 +22,11 @@ router.post(
       });
       console.log("Found event:", event);
       if (!event) {
-        return res
-          .status(400)
-          .json({ error: "Voting is currently not allowed." });
+        return res.status(400).json({ error: "Voting is currently not allowed." });
       }
       const candidate = event.candidates.id(candidateId);
       if (!candidate) {
-        return res
-          .status(404)
-          .json({ error: "Candidate not found in the event" });
+        return res.status(404).json({ error: "Candidate not found in the event" });
       }
       // const candidateIds = event.candidates.map((cand: any) => cand._id.toString());
       const eventIdStr = event._id.toString();
@@ -41,9 +35,7 @@ router.post(
         eventId: eventIdStr,
       });
       if (existingVote) {
-        return res
-          .status(400)
-          .json({ error: "You have already voted in this Event." });
+        return res.status(400).json({ error: "You have already voted in this Event." });
       }
       console.log(eventIdStr);
       console.log(candidateId);
@@ -52,9 +44,7 @@ router.post(
         await newVote.save();
       } catch (err: any) {
         if (err.code === 11000) {
-          return res
-            .status(400)
-            .json({ error: "You have already voted in this event." });
+          return res.status(400).json({ error: "You have already voted in this event." });
         }
         throw err;
       }
@@ -64,16 +54,12 @@ router.post(
         { new: true }
       );
       if (!updatedEvent) {
-        return res
-          .status(500)
-          .json({ error: "Error updating candidate votes" });
+        return res.status(500).json({ error: "Error updating candidate votes" });
       }
 
       const updatedCandidate = updatedEvent.candidates.id(candidateId);
       if (!updatedCandidate) {
-        return res
-          .status(500)
-          .json({ error: "Candidate not found in the event after update" });
+        return res.status(500).json({ error: "Candidate not found in the event after update" });
       }
       // const newVote = new Voter({ voterId, categoryId: categoryId});
       // await newVote.save();
